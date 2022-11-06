5620540f68cb8182bd2c82baf65733d3f0d293b2
Pedro <calixtops@github.com>
2022-11-06 11:22:10 -0300

    .

--- pages/home.py
+++ pages/home.py
@@ -10,8 +10,12 @@ layout = html.Div([
 
 
 
-    dcc.Markdown('''# Pedro Silveira Calixto  [![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/calixtops/)''', style={'textAlign':'center','display':'inline'}),
+    dcc.Markdown('''# Pedro Silveira Calixto''', 
+        style={'textAlign':'center','display':'inline'}),
 
+
+    dcc.Markdown('''[![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/calixtops/) [![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)](https://github.com/calixtops)''', 
+        style={'textAlign':'center','display':'inline'}),
 
 
     dcc.Markdown('### Resumo Profissional', style={'textAlign': 'center'}),
